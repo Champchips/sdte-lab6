37adfcd1efa24f3a771de4f43ab4c870bc7a149d
Champchips <43027845+Champchips@users.noreply.github.com>
2021-02-22 15:37:34 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/hi", (req, res) => {
-  res.send("Test to Failure on PR");
+  res.send("Hello world!");
 });
 
 module.exports = app;
