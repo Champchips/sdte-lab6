969ec52664212355355db56a9644420a229b8f9f
Champchips <otakuchamp@gmail.com>
2021-02-22 15:10:45 +0700

    Test to Failure

--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/hi", (req, res) => {
-  res.send("Hello world!");
+  // res.send("Hello world!");
+  return "Test to Failure"
 });
 
 module.exports = app;
